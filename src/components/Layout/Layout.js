4b9f890fd5651373570d4edc360d7737f7c18d72
nada tarek  galal <nadagalal1995@gmail.com>
2020-06-08 02:06:56 +0200

    Burger and burgerIngredient components,Install proptype package

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Auxiliary from '../../hoc/Auxiliary'
+import classes from './Layout.css'
 const layout = (props) => (
     <Auxiliary>
         <div > toolbar,slideDrawer,backdrop </div>
-        <main>
+        <main className={classes.Content}>
           {props.children}
         </main>
     </Auxiliary>
